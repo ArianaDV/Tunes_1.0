f4174f5c6f62596098b541d5cad376836435cb5c
Anton Mykhaylov <>
2018-05-11 16:37:15 -0500

    merging css styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,9 +7,7 @@ class Home extends Component {
     render(){
         return(
         <div>
-        <div>
-        <Link to={'/playlist'}><h2>Influence your party</h2></Link>
-        </div>
+        <h2>WELCOME</h2>
 
         <div className="fade"></div>
         <section className="star-wars">
@@ -19,8 +17,8 @@ class Home extends Component {
             <p>Episode IV</p>
             <h1>A New Hope</h1>
           </div>
-          <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>     
-          <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
+          <p>DRAKE Childish Gambino Rich the KID Playboi Carti Famous DEX Wiz Khalifa Gucci Mane Swae Lee Rae Sremmurd Desiigner  Migos Nicki Minaj Murda Beetz Belly Lil Xan Lil Yachty Kanye West</p>     
+          <p>Charlie Puth Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
           <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
         </div>
       </section>
