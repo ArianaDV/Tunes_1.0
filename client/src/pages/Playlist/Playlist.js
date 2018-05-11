640de50bdc007bf3130de5e69828b8313307aa1c
eric2esca <eric2esca@gmail.com>
2018-05-11 00:52:06 -0500

    deleted search bar and made one col-12 wrapper

--- client/src/pages/Playlist/Playlist.js
+++ client/src/pages/Playlist/Playlist.js
@@ -6,24 +6,16 @@ class Playlist extends Component {
     render(){
         return(
             <div className="container">
-            <h1>Find your jam...</h1>
-            <form method="get" action="?">
-            <input type="text" placeholder="Search" />
-            <ul class="suggestions"></ul>
-            <button type="submit">
-            <i class="fa fa-search"></i>
-            </button>
-            </form>
-            <div className="row">
-           <div className="col-sm-12 col-md-6 col-lg-6 mt-4">
-               <div className="card">
-                   <h1>Spotify API goes here</h1>
-               </div>
-           </div>
-           <div className="col-sm-12 col-md-6 col-lg-6 mt-4">
-               {/* <Panel /> */}
-           </div>
-            </div>  
+                <h1>Find your jam...</h1>
+                <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12 mt-4">
+                        <div className="card">
+                            <h1>Spotify API goes here</h1>
+                            
+                        </div>
+                    </div>
+            
+                </div>  
    
             </div>
 
