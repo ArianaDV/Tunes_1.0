ed35888794b6de73ade91c766c7458c99ae969c0
eric2esca <eric2esca@gmail.com>
2018-05-11 01:36:54 -0500

    added new filter search 2. added function that console.log data on click 3. added fetch commands to get information from user spotify account 4. used fetch command to get track data as well

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -4,53 +4,13 @@ import axios from 'axios';
 import './style.css'
 
 class Login extends Component {
-  // login = (e) => {
-  //   var userData = { email: this.refs.loginemail.value, password: this.refs.signinpassword.value };
-  //       if (!userData.email || !userData.password) {
-  //         alert('Please enter email and password.')
-  //         return;
-  //       }
-
-  //   this.refs.loginemail.value = "";
-  //   this.refs.signinpassword.value = "";
-
-  //   axios
-  //     .post("/api/login", { email: userData.email, password: userData.password })
-  //     .then(function(response) {
-  //       window.location.replace(response.data.redirect);
-  //       console.log(response)
-  //     })
-  //     .catch(function(error) {
-  //       console.log("error is" + error);
-  //     });
-
-
-
-  
 
   render(){
     return (
       <div>
-        {/* <div> className="form-signin" name="signin" id="signupForm-sidebar">       */}
-        {/* <form className="login">
-        <h2 className="form-signin-heading">Login</h2>
-        <div className="form-group">
-          <label className="form-spacing" for="exampleInputEmail1">Email address</label><br/>
-          <input type="email" className="form-control form-spacing" id="email-input1" ref="loginemail" name="email" placeholder="Email" required />
-        </div>
-        <div className="form-group">
-          <label className="form-spacing" for="exampleInputPassword1">Password</label><br/>
-          <input type="password" className="form-control form-spacing" id="password-input1" ref="signinpassword" name="password" placeholder="Password" required />
-        </div>
-        <button className="btn btn-default" id="signin" onClick={this.login}>
-          Login
-        </button>
-        <br />
-        <p>Or sign up <a href="/signup">here</a></p>
-        </form> */}
 
         <button class='btn' onClick={() => {
-          true// window.location= window.location.href.includes('localhost')''
+          true
             ? window.location.replace("https://murmuring-shore-84467.herokuapp.com/login")  // 'http://localhost:8888/login'
             : 'error'
         }
--- client/src/pages/Playlist/Playlist.js
+++ client/src/pages/Playlist/Playlist.js
@@ -1,7 +1,74 @@
 import React, { Component } from "react";
+import queryString from 'query-string';
+import Filter from "../../components/Filter/Filter";
+import axios from 'axios';
+import './style.css'
 
 
 class Playlist extends Component {
+
+    state = {
+        field: {},
+        filterString: '',
+        songData: '',
+        songUrl: '',
+        songImage: '',
+        songArtist: '',
+        songID: '',
+        user: ''
+    }
+
+    onSubmit = field => {
+        let parsed = queryString.parse(window.location.search);
+        let accessToken = parsed.access_token;
+        if (!accessToken)
+            return;
+
+        this.setState({ field });
+
+        fetch('https://api.spotify.com/v1/search?q=' + field.songSearch + '&type=track', {
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).then(response => response.json())
+            .then(data => {
+                let songData = data.tracks.items[0];
+                let songUrl = data.tracks.items[0].external_urls;
+                let songImage = songData.album.images[0].url;
+                let songArtist = songData.artists[0].name;
+                let songID = songData.id;
+                console.log('Title: ', songData.name)
+                console.log('Artist: ', songArtist)
+                console.log('song URL ', songUrl)
+                console.log('songImage ', songImage)
+                console.log('------------------------------------------------------------------------')
+
+
+            })
+    }
+
+
+    componentDidMount(){
+        let parsed = queryString.parse(window.location.search);
+        let accessToken = parsed.access_token;
+        if (!accessToken)
+            return;
+
+        //This gets the users spotify profile information------------------
+        fetch('https://api.spotify.com/v1/me', {
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let songID = data.id;
+                console.log(songID);
+                console.log(this.state)
+                this.setState({
+                    songID: songID
+                })
+                console.log(this.state.songID)
+            })
+        //----------------------------------------------------------------
+
+    }
 
     render(){
         return(
@@ -11,7 +78,7 @@ class Playlist extends Component {
                     <div className="col-sm-12 col-md-12 col-lg-12 mt-4">
                         <div className="card">
                             <h1>Spotify API goes here</h1>
-                            
+                            <Filter onSubmit={field => this.onSubmit(field)} />
                         </div>
                     </div>
             
