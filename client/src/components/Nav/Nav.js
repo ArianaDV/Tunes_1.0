cb5d3d82f3dd7dd2b90c590cd585151c6864558b
Anton Mykhaylov <>
2018-05-11 14:46:51 -0500

    css cleanup

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -10,10 +10,10 @@ class Nav extends Component {
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse1" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <Link to="/" className="nav-brand"><i className="fa fa-headphones"></i></Link>
+      <Link to="/" className="nav-brand"><i className="fa fa-headphones" id="headp"></i></Link>
       <div className="collapse navbar-collapse" id="navbarCollapse1">
         <ul className="navbar-nav ml-auto">
-        <li className="nav-item">  <Link to="/login" className="nav-brand">Login</Link>
+        <li className="nav-item">  <Link to="/login" className="nav-brand"><i className="fa fa-sign-in"></i></Link>
         </li>
         </ul>
       </div>
